8a04430fb46161f06ba7f120a11ba948bd6e3680
zaalTsagareli <zaali.tsagareli@makingscience.com>
2023-06-01 00:18:53 +0400

    changed little bit of transport interface

--- src/interfaces/rpc.ts
+++ src/interfaces/rpc.ts
@@ -1,30 +1,34 @@
-export enum Mode { Request, Notify, Ping }
+export enum Mode {
+  Request,
+  Notify,
+  Ping,
+}
 
 export interface JSONRPCRequest {
-    mode: Mode,
-    jsonrpc: string;
-    method: string;
-    params?: any[];
-    id?: string | number | null;
+  mode: Mode;
+  jsonrpc: string;
+  method: string;
+  params?: any[];
+  id?: string | number | null;
 }
 
 export interface JSONRPCResponse {
-    jsonrpc: string;
-    result?: any;
-    error?: {
-        code: number;
-        message: string;
-        data?: any;
-    };
-    id: string | number | null;
+  jsonrpc: string;
+  result?: any;
+  error?: {
+    code: number;
+    message: string;
+    data?: any;
+  };
+  id: string | number | null;
 }
 
 export interface JSONRPCHandler {
-    [methodName: string]: (...params: any[]) => Promise<any>;
+  [methodName: string]: (...params: any[]) => Promise<any> | any;
 }
 
 export interface Transport {
-    delete(): unknown;
-    onData(callback: (data: string) => void): void;
-    send(data: string): Promise<JSONRPCResponse>;
+  delete(): void;
+  onData(handler: Function);
+  start(): Promise<void>;
 }
